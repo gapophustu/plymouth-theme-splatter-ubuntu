23b8b71836b582bca8797791f5604e5d6c4132d5
gapophustu <>
2018-04-23 20:56:22 +0200

    code cleanup

--- convert.sh
+++ convert.sh
@@ -1,41 +1,42 @@
 #!/bin/bash
 
-k=0
+what=$(ls -A | grep -E ".xcf$" | cut -f2 -d "-" | cut -f1 -d ".")
 
-if [[ ! -f splatter-ubuntu/shutdown_5_0.png ]]
+if [[ ! -f splatter-$what/shutdown_5_0.png ]]
 then
-    cp -a splatter-ubuntu.png splatter-ubuntu/shutdown_5_0.png
+    cp -a splatter-$what.png splatter-$what/shutdown_5_0.png
 fi
 
-convert splatter-ubuntu.png -alpha extract mask.png
+convert splatter-$what.png -alpha extract mask.png
 for j in $(ls -A | grep "shutdown_" | sort -g)
 do
-    output=$(echo splatter-ubuntu/$(echo shutdown_5_$(echo $j | cut -c10-)))
+    output=$(echo splatter-$what/$(echo shutdown_5_$(echo $j | cut -c10-)))
     if [[ ! -f $output ]]
     then
         convert mask.png $j -composite temp.png
-        convert splatter-ubuntu.png temp.png -alpha Off -compose CopyOpacity -composite $output
+        convert splatter-$what.png temp.png -alpha Off -compose CopyOpacity -composite $output
     fi
 done
 rm mask.png temp.png
 
+k=0
 for i in $(cat sizes)
 do
     k=$(expr $k + 1)
-    for j in $(ls -A splatter-ubuntu/ | grep "splatter_5" | sort -g)
+    for j in $(ls -A splatter-$what/ | grep "splatter_5" | sort -g)
     do
-        output=$(echo splatter-ubuntu/$(echo splatter_$(echo $k)_$(echo $j | cut -c12-)))
+        output=$(echo splatter-$what/$(echo splatter_$(echo $k)_$(echo $j | cut -c12-)))
         if [[ ! -f $output ]]
         then
-            convert splatter-ubuntu/$j -resize $i $output
+            convert splatter-$what/$j -resize $i $output
         fi
     done
-    for j in $(ls -A splatter-ubuntu/ | grep "shutdown_5" | sort -g)
+    for j in $(ls -A splatter-$what/ | grep "shutdown_5" | sort -g)
     do
-        output=$(echo splatter-ubuntu/$(echo shutdown_$(echo $k)_$(echo $j | cut -c12-)))
+        output=$(echo splatter-$what/$(echo shutdown_$(echo $k)_$(echo $j | cut -c12-)))
         if [[ ! -f $output ]]
         then
-            convert splatter-ubuntu/$j -resize $i $output
+            convert splatter-$what/$j -resize $i $output
         fi
     done
 done
